e3f900f6ce3feff3769be7b46ec7244f289826aa
bpasn <boyweihey_love@hotmail.com>
2023-02-27 17:59:07 +0700

    no message

--- services/config/default.ts
+++ services/config/default.ts
@@ -1,6 +1,6 @@
 export default {
     formatLogRequest: `:url  [:date[clf]] :method / HTTP/:http-version Body: :body :params :query :remote-addr - :remote-user`,
-    port: 3000,
+    port: 3001,
     connectionString: "mongodb://swagger:1234@localhost:27018/swagger?retryWrites=true&w=majority",
     accessTokenPrivateKey: "",
     refreshTokenPrivateKey: "",
